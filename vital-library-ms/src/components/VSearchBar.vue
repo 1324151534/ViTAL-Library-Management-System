0b83eb6596b115cb7b2c925389b46e2da3dbead0
xjimlinx <1021538027@qq.com>
2024-04-10 11:55:43 +0800

    Add main pages && Router && Some Components

---
+++ vital-library-ms/src/components/VSearchBar.vue
@@ -0,0 +1,33 @@
+<script setup>
+import VFonts from "@/components/VFonts.vue";
+</script>
+
+<template>
+    <div class="SearchBar-box"><input type="text" placeholder="Search The Book You Want"><button
+            type="submit" class="search-button">Search</button></div>
+</template>
+
+<style>
+.SearchBar-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 50px;
+    font-family: "MiSans";
+}
+
+.search-button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 5px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    font-family: "MiSans";
+}
+</style>
